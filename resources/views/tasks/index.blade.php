0997b2c79266eacb39eff7973080ef8609dda2b6
nozomi.ushijima <yrznet217113@yahoo.co.jp>
2020-08-19 11:30:23 +0000

    error correction

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,15 +4,7 @@
 
     @if (Auth::check())
         <h1>タスク一覧</h1>
-    @else
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>Welcome to the Tasklist</h1>
-                {{-- ユーザー登録ページへのリンク --}}
-                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
-            </div>
-        </div>
-    
+        
         @if (count($tasks) > 0)
             <table class="table table-striped">
                 <thead>
@@ -36,7 +28,16 @@
         @endif
         
         {{-- タスク作成ページへのリンク --}}
-        {{-- {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!} --}}
+        {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
+    
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Tasklist</h1>
+                {{-- ユーザー登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
         
     @endif
     
